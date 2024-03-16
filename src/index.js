2bc6a64220f524b687ddd17a1b1da13ac0378ece
deal-machine <95deal@gmail.com>
2024-03-16 12:20:50 -0300

    feat: add generation token on serverless function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-exports.handler = (_, res) => res.send('Hello, World!');
+import axios from 'axios'
+import jwt  from 'jsonwebtoken'
+
+const CONSUMER = `orderly`
+const AUTH_URL = `http://localhost/kong/config`
+exports.handler = async (_, res) => {
+    try {
+        const url = `${AUTH_URL}/consumers/${CONSUMER}/jwt`
+        const { data } = await axios.get(url, {})
+
+        const key = data.data[0].key        
+        const secret = data.data[0].secret
+
+        const token = jwt.sign({}, secret, { keyid: key, expiresIn: "1d"  })
+
+        res.status(200).json({token, secret})
+    } catch(error) {
+        return res.status(500).json(error)
+    }   
+}
