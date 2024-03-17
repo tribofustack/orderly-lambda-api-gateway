64af7f386b193865694edf629e8aaafc10d98b16
PedroQuessada <pedro.rafael.quessada@gmail.com>
2024-03-17 14:14:09 -0300

    add permission

--- modules/serverless/main.tf
+++ modules/serverless/main.tf
@@ -4,6 +4,38 @@ data "archive_file" "function_zip" {
   output_path = "${path.module}/function.zip"
 }
 
+resource "google_project_iam_member" "artifact_registry_reader" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_cloudfunctions_function.default.service_account_email}"
+}
+
+resource "google_project_iam_member" "artifact_registry_list" {
+  project = var.project_id
+  role    = "roles/artifactregistry.list"
+  member  = "serviceAccount:${google_cloudfunctions_function.default.service_account_email}"
+}
+
+# resource "google_project_iam_custom_role" "artifact_registry_access" {
+#   role_id     = "cloudfunctions_artifact_registry_access"
+#   title       = "Cloud Functions Artifact Registry Access"
+#   description = "Custom role to grant access to Artifact Registry for Cloud Functions"
+#   permissions = [
+#     "artifactregistry.repositories.reader",
+#     "artifactregistry.repositories.list",
+#     "artifactregistry.repositories.get",
+#     "artifactregistry.packages.download"
+#   ]
+# }
+
+# resource "google_project_iam_binding" "function_service_account_binding" {
+#   project = var.project_id
+#   role    = google_project_iam_custom_role.artifact_registry_access.role_id
+#   members = [
+#     "serviceAccount:${google_cloudfunctions_function.default.service_account_email}"
+#   ]
+# }
+
 resource "google_storage_bucket_object" "function_zip" {
   name   = "function.zip"
   bucket = var.bucket_name
@@ -19,7 +51,10 @@ resource "google_cloudfunctions_function" "default" {
   source_archive_object = google_storage_bucket_object.function_zip.name
   trigger_http          = true
   entry_point           = "handler"
-  depends_on            = [google_storage_bucket_object.function_zip]
+  depends_on            = [
+    # google_project_iam_binding.function_service_account_binding,
+    google_storage_bucket_object.function_zip
+  ]
 
   environment_variables = {
     CONSUMER = var.consumer
