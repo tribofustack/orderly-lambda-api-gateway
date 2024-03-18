de699e30cbe5de4b09a7af6514098ab77bf40ef4
arthursvpb <arthurandroid15@gmail.com>
2024-03-18 17:28:58 -0300

    add isCPFValid function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 
+const { isValidCPF } = require('./utils/cpf');
+
 const { CONSUMER, AUTH_URL } = process.env;
 
-exports.handler = async (_, res) => {
+exports.handler = async (req, res) => {
   try {
     if (!CONSUMER || !AUTH_URL)
-      throw new Error('Please check your environment configuration.');
+      throw new Error('Missing environment configuration.');
+
+    const { cpf } = req.body;
+    const isCPFValid = isValidCPF(cpf);
+
+    if (cpf && !isCPFValid)
+      return res.status(400).json({ error: 'Invalid CPF.' });
 
     const url = `${AUTH_URL}/consumers/${CONSUMER}/jwt`;
-    const { data: response } = await axios.get(url, {});
-
+    const { data: response } = await axios.get(url);
     const [{ key, secret }] = response.data;
-    const token = jwt.sign({}, secret, { keyid: key, expiresIn: '1d' });
+
+    const payload = { isCPFValid };
+    const token = jwt.sign(payload, secret, { keyid: key, expiresIn: '1d' });
 
     return res.status(200).json({ token });
   } catch (error) {
-    return res.status(500).json(error);
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error.' });
   }
 };
